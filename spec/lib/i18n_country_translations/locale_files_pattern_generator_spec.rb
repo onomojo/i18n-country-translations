a34fecfef6720cf160210100a953520c9581a090
Michiel de Wit <mail@michieldewit.nl>
2015-10-20 11:33:49 +0200

    Made test suitable for use in Ruby 2.0 by compiling glob pattern into rudimentary regular expression

--- spec/lib/i18n_country_translations/locale_files_pattern_generator_spec.rb
+++ spec/lib/i18n_country_translations/locale_files_pattern_generator_spec.rb
@@ -5,18 +5,40 @@ describe I18nCountryTranslations::LocaleFilesPatternGenerator do
   context "with given locale" do
     let(:generator) { I18nCountryTranslations::LocaleFilesPatternGenerator.new('base/', '.yml') }
 
+    def test_pattern(pattern, sample)
+      if defined? File::FNM_EXTGLOB
+        # Use proper way to test, by calling fnmatch and actually executing the pattern.
+        # This only works in Ruby 2.1.0 and upwards
+        File.fnmatch(pattern, sample, File::FNM_EXTGLOB)
+      else
+        # Roughly approximate pattern matching using regular expressions. Only recognizes the braces { } syntax.
+        # Other glob syntax (e.g. * and ** is not recognized). So keep test cases simple!
+        pattern = '^' + pattern.split(/\{(.+?)\}/).each_with_index.map do |part, index|
+          if index.even?
+            # Pattern part outside of { }
+            Regexp.escape(part)
+          else
+            '(' + part.split(',').map { |option| Regexp.escape(option) }.join('|') + ')'
+          end
+        end.join + '$'
+
+        # Test against regular expression pattern
+        (sample =~ Regexp.new(pattern)) == 0
+      end
+    end
+
     it "generates a pattern for simple locales" do
       locales = ['en', 'fr']
       pattern = generator.pattern_from(locales)
 
       # Make sure pattern matches locales
       locales.each do |locale|
-        expect(File.fnmatch(pattern, "base/#{locale}.yml", File::FNM_EXTGLOB)).to be true
+        expect(test_pattern(pattern, "base/#{locale}.yml")).to be true
       end
 
       # Make sure pattern does not match other locales
       ['nl', 'en-US', 'fra', 'cen'].each do |locale|
-        expect(File.fnmatch(pattern, "base/#{locale}.yml", File::FNM_EXTGLOB)).to be false
+        expect(test_pattern(pattern, "base/#{locale}.yml")).to be false
       end
     end
 
@@ -27,12 +49,12 @@ describe I18nCountryTranslations::LocaleFilesPatternGenerator do
 
       # Make sure pattern matches locales
       sublocales.each do |locale|
-        expect(File.fnmatch(pattern, "base/#{locale}.yml", File::FNM_EXTGLOB)).to be true
+        expect(test_pattern(pattern, "base/#{locale}.yml")).to be true
       end
 
       # Make sure pattern does not match other locales
       ['en-AU', 'az-IR'].each do |locale|
-        expect(File.fnmatch(pattern, "base/#{locale}.yml", File::FNM_EXTGLOB)).to be false
+        expect(test_pattern(pattern, "base/#{locale}.yml")).to be false
       end
     end
   end
